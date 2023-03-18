0a02f9549bb59beab0e3cd2a7a279fb22ad32102
RoKrama <111682946+RoKrama@users.noreply.github.com>
2023-03-18 05:02:24 +0100

    working alpha changed many things no more std::barier own implementation now but should be changed control flow defined with mutexes and std::condition_variable-s tested very lightly, race condition faliures still possible

--- Maneger.cpp
+++ Maneger.cpp
@@ -1,16 +1,36 @@
 #include "Maneger.h"
+using namespace opt;
+
+int main()
+{
+
+	std::function<void(int)> func( // any function here to be executed with threads n_data times
+		[](int i) noexcept {std::cout << i << std::endl; }
+	);
+	OPTManeger<std::function<void(int)>> 
+		man(
+			4, // n_threads
+			50, // n_data
+			func // function
+	);
+
+	for (int i = 0; i < 100; i++)
+		man.run_and_collect();
+
+	return 0;
+}
+
 template <typename Fn_type>
 OPTManeger<Fn_type>::OPTManeger(const int n_threads, const int n_data, Fn_type& in_fn) :
 	N_THREADS(n_threads),
 	N_DATA(n_data),
 	data_idx(0),
 	threads(),
-	w_lock(), cv_mtx(),
+	write_lock(), finish_lock(), start_lock(),
 	c_v(),
 	sync_finish_complete(),
 	start_promise(), start_future(),
-	sync_finish(nullptr),
-	processing_done(false), can_begin(false), stop(false),
+	processing_done(false), can_begin(false), stop_threading(false),
 	worker(in_fn)
 {
 	start_future = start_promise.get_future();
@@ -20,77 +40,76 @@ OPTManeger<Fn_type>::OPTManeger(const int n_threads, const int n_data, Fn_type& 
 template <typename Fn_type>
 void OPTManeger<Fn_type>::init()
 {
-
-	sync_finish_complete = [this]() mutable noexcept
+	std::function<void()> begin = [this]() mutable noexcept
 	{
-		std::unique_lock u_lock(cv_mtx);
-		processing_done = true; // set flags
-		can_begin = false; // prevent from running immedeately again
+		std::unique_lock<std::mutex> u_lock(start_lock); // wait until main frees
+		c_v.wait(u_lock, [=]() ->bool {return can_begin || stop_threading; }); // check flags
 		data_idx = 0; // reset data "quee"
-		u_lock.unlock(); // unlock before notify
+	};
+
+	// sync before begining of computation and wait for main to signal start or stop
+	sync_begin = new opt::gates<std::function<void()>>(N_THREADS, begin);
+
+	std::function<void()> complete = [this]() mutable noexcept
+	{
+		processing_done = true; // set flags
+		can_begin = false; // prevent from running immedeately again and give control to main
 		c_v.notify_one(); // notify waiting main we're done
+		std::unique_lock u_lock(finish_lock); // block till main unlocks
 	};
 
 	// sync after completing and wait to collect()
-	sync_finish = new std::barrier<std::function<void()>>(N_THREADS, sync_finish_complete);
+	sync_finish = new opt::gates<std::function<void()>>(N_THREADS, complete);
 
-	
+
 	for (int i = 0; i < N_THREADS; i++)
 		threads.push_back(
 			std::thread(
-				[this]() mutable noexcept
+				[&, i]() mutable noexcept
 				{
 					int thread_data_idx; // for saving data index before it gets mutated 
 					start_future.wait(); // wait for main to signal start
-
+					int t_idx = i;
 					while (true) // running loop
 					{
-						std::unique_lock<std::mutex> u_lock(cv_mtx);
-						c_v.wait(u_lock, [] { return can_begin || stop_threading; }); // wait to get signal to go again or stop
-						u_lock.unlock();
-						if (stop_threading) break; // stop running
-						while (true) // processing loop
+						sync_begin->arrive_and_wait(); // wait to get signal to go again or stop
+						if (stop_threading) return; // as threads are synced none should get caught in sync_finish
+						while (true) // process loop
 						{
-							w_lock.lock(); // lock before getting data index
-							if ((thread_data_idx = data_idx) < N_DATA) // set and check if we're in data range
-							{
-								data_idx++; // increment index for next thread
-								w_lock.unlock(); // no more mutations of shared variables
+							write_lock.lock(); // lock before getting data index
+							thread_data_idx = data_idx++; // read and increment index for next thread
+							write_lock.unlock(); // no more mutations of shared variables
+							if (thread_data_idx < N_DATA)
 								worker(thread_data_idx); // call worker to process data
-							}
-							else // no more data to compute
-							{
-								w_lock.unlock();
-								break;
-							}
-						} // processing loop
+							else break;
+						} // process loop
+
 						sync_finish->arrive_and_wait(); // wait for all threads to come here and signal finish to main
 					} // running loop
 				}
 			)
 		);
-
 	start_promise.set_value(); // signal to start
 }
 template <typename Fn_type>
 void OPTManeger<Fn_type>::run_and_collect()
 {
-	std::unique_lock<std::mutex> u_lock(cv_mtx);
-	can_begin = true; // beging processing flag
-	processing_done = false; // reset
-	u_lock.unlock();
-	c_v.notify_all(); // signal
 
-	c_v.wait(u_lock, [] { return processing_done || stop_threading; });
+	std::unique_lock<std::mutex> u2_lock(finish_lock); // control exit before threads acqure it 
+	can_begin = true; // by controlling beging processing flag
+	processing_done = false; // needs a reset here or it will race it
+	c_v.notify_one(); // signal start
 
+	// threads start
+	c_v.wait(u2_lock, [this] { return processing_done || stop_threading; });
+	u2_lock.unlock(); //  unlock completion
+	c_v.notify_one();
 }
 template <typename Fn_type>
 void OPTManeger<Fn_type>::stop()
 {
-	std::unique_lock<std::mutex> u_lock(cv_mtx);
 	stop_threading = true; // beging processing flag
-	u_lock.unlock();
-	c_v.notify_all(); // signal
+	c_v.notify_all(); // signal dropout
 	for (std::thread& thread : threads)
 		if (thread.joinable()) thread.join();
 }
@@ -98,5 +117,45 @@ template <typename Fn_type>
 OPTManeger<Fn_type>::~OPTManeger()
 {
 	stop();
+	delete sync_begin;
 	delete sync_finish;
 }
+
+template <typename Fn_type>
+gates<Fn_type>::gates(int expected, Fn_type in_fn) :
+	arrived_count(0),
+	N_EXPECTED(expected),
+	complete(in_fn),
+	completion_fn_done(false),
+	thread_lock(), read_lock()
+{}
+
+template <typename Fn_type>
+void gates<Fn_type>::arrive_and_wait()
+{
+	read_lock.lock();
+	int now_count = ++arrived_count;
+	read_lock.unlock();
+
+	if (now_count == N_EXPECTED)
+	{
+		complete();
+		arrived_count--;
+		completion_fn_done = true;
+		c_v.notify_all();
+	}
+	else if (now_count < N_EXPECTED)
+	{
+		std::unique_lock<std::mutex> u_lock(thread_lock);
+		c_v.wait(u_lock, [this]() ->bool {return completion_fn_done; });
+		if (--arrived_count <= 0)
+		{
+			arrived_count = 0; // case only one thread
+			completion_fn_done = false; // resetwith last thread
+		}
+	}
+
+}
+
+
+
